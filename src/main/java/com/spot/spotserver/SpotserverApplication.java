427a9fb1f328e2302cd9577b96f71a8b5560c446
Jeongmin39 <wjdals20309@naver.com>
2024-08-17 22:56:46 +0900

    fix: JPA Auditing 기능 활성화

--- src/main/java/com/spot/spotserver/SpotserverApplication.java
+++ src/main/java/com/spot/spotserver/SpotserverApplication.java
@@ -2,7 +2,10 @@ package com.spot.spotserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpotserverApplication {
 
