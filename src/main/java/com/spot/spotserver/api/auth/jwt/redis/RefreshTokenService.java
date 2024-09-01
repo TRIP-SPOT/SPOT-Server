c875b1b348172da4cdc9e3a6af3bcbb6aeea618f
Jeongmin39 <wjdals20309@naver.com>
2024-09-01 22:28:02 +0900

    refac: 리프레시 토큰 id type 변경에 따른 코드 수정

--- src/main/java/com/spot/spotserver/api/auth/jwt/redis/RefreshTokenService.java
+++ src/main/java/com/spot/spotserver/api/auth/jwt/redis/RefreshTokenService.java
@@ -4,8 +4,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @RequiredArgsConstructor
 @Service
 public class RefreshTokenService {
@@ -16,7 +14,6 @@ public class RefreshTokenService {
     public void saveRefreshToken(final Long userId, final String refreshToken) {
         // 기존 리프레시 토큰 삭제
         deleteRefreshToken(userId);
-
         // 새로운 리프레시 토큰 저장
         tokenRepository.save(Token.of(userId, refreshToken));
     }
@@ -29,11 +26,10 @@ public class RefreshTokenService {
         return token.getId();
     }
 
-    @Transactional
     public void deleteRefreshToken(final Long userId) {
-        Optional<Token> token = tokenRepository.findById(userId);
-        if (token.isPresent()) {
-            tokenRepository.delete(token.get());
+        String strUserId = userId.toString();
+        if (tokenRepository.existsById(strUserId)) {
+            tokenRepository.deleteById(strUserId);
         } else {
             System.out.println("삭제할 리프레시 토큰이 존재하지 않습니다. 사용자 ID: " + userId);
         }
