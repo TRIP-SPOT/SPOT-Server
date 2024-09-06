bb7c941678c2e67c34f6d5468fcc94b32ce6dd14
Jeongmin39 <wjdals20309@naver.com>
2024-09-06 17:15:10 +0900

    setting: SuccessCode 추가

--- src/main/java/com/spot/spotserver/common/payload/SuccessCode.java
+++ src/main/java/com/spot/spotserver/common/payload/SuccessCode.java
@@ -14,7 +14,8 @@ public enum SuccessCode {
     // 200 OK
     GET_SPOT_SUCCESS(OK, "촬영지 조회에 성공하였습니다."),
     LOGIN_SUCCESS(OK, "로그인에 성공하였습니다"),
-    REISSUE_TOKEN_SUCCESS(OK, "토큰 재발급에 성공하였습니다.");
+    REISSUE_TOKEN_SUCCESS(OK, "토큰 재발급에 성공하였습니다."),
+    REGISTER_NICKNAME_SUCCESS(OK, "닉네임이 정상적으로 설정되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
