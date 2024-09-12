11f14bdf8fcc56fa2856ec188a19ad57a821d9f3
yang <hc1489@naver.com>
2024-09-12 20:02:34 +0900

    feat: RecordImageRepository에 썸네일 이미지 조회 기능 추가

--- src/main/java/com/spot/spotserver/api/record/repository/RecordImageRepository.java
+++ src/main/java/com/spot/spotserver/api/record/repository/RecordImageRepository.java
@@ -1,7 +1,11 @@
 package com.spot.spotserver.api.record.repository;
 
+import com.spot.spotserver.api.record.domain.Record;
 import com.spot.spotserver.api.record.domain.RecordImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RecordImageRepository extends JpaRepository<RecordImage, Long> {
+    Optional<RecordImage> findFirstByRecordOrderByIdAsc(Record record);
 }
