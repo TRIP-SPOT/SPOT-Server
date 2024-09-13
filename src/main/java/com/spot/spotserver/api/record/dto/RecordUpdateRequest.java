22abf719dc6047a05d2184dc81a8e2eec85a62cc
yang <hc1489@naver.com>
2024-09-13 18:48:29 +0900

    feat: RecordUpdateRequest DTO 작성

---
+++ src/main/java/com/spot/spotserver/api/record/dto/RecordUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.spot.spotserver.api.record.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class RecordUpdateRequest {
+    String title;
+
+    String description;
+
+    List<Long> deleteImages;
+}
