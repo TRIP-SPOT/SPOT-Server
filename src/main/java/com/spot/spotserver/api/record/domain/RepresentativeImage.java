549476c4335ed699504a60ece4e4f17f106fcbce
yang <hc1489@naver.com>
2024-09-14 15:59:22 +0900

    feat: 대표 이미지 Entity 작성

---
+++ src/main/java/com/spot/spotserver/api/record/domain/RepresentativeImage.java
@@ -0,0 +1,32 @@
+package com.spot.spotserver.api.record.domain;
+
+import com.spot.spotserver.api.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "representative_image")
+@Getter
+@NoArgsConstructor
+public class RepresentativeImage {
+
+    @GeneratedValue @Id
+    private Long id;
+
+    private String url;
+
+    private Region region;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public RepresentativeImage(String url, Region region, User user) {
+        this.url = url;
+        this.region = region;
+        this.user = user;
+    }
+}
