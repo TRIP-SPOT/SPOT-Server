fbffb82867127f0a6fb03bccedb1ee2f57ea7b8c
yang <hc1489@naver.com>
2024-09-14 16:04:08 +0900

    feat: 대표 이미지 생성 로직 구현

---
+++ src/main/java/com/spot/spotserver/api/record/service/RepresentativeImageService.java
@@ -0,0 +1,30 @@
+package com.spot.spotserver.api.record.service;
+
+import com.spot.spotserver.api.record.domain.RepresentativeImage;
+import com.spot.spotserver.api.record.dto.RepresentativeImageRequest;
+import com.spot.spotserver.api.record.dto.RepresentativeImageResponse;
+import com.spot.spotserver.api.record.repository.RepresentativeImageRepository;
+import com.spot.spotserver.api.user.domain.User;
+import com.spot.spotserver.common.s3.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class RepresentativeImageService {
+    private final RepresentativeImageRepository representativeImageRepository;
+    private final S3Service s3Service;
+
+    public RepresentativeImageResponse createRepresentativeImage(RepresentativeImageRequest representativeImageRequest, User user) throws IOException {
+        String representativeImageUrl = this.s3Service.upload(representativeImageRequest.getImage(), user.getNickname());
+        RepresentativeImage newRepresentativeImage = RepresentativeImage.builder().region(representativeImageRequest.getRegion())
+                .url(representativeImageUrl)
+                .user(user)
+                .build();
+
+        RepresentativeImage savedRepresentativeImage = this.representativeImageRepository.save(newRepresentativeImage);
+        return new RepresentativeImageResponse(savedRepresentativeImage);
+    }
+}
