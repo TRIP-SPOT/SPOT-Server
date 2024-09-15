fcfeeb58a4c044844f1c5bc1bb4e66b35435a8e8
yang <hc1489@naver.com>
2024-09-15 20:28:45 +0900

    feat: 대표 이미지 수정 기능 구현

--- src/main/java/com/spot/spotserver/api/record/service/RepresentativeImageService.java
+++ src/main/java/com/spot/spotserver/api/record/service/RepresentativeImageService.java
@@ -8,16 +8,20 @@ import com.spot.spotserver.api.user.domain.User;
 import com.spot.spotserver.common.s3.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RepresentativeImageService {
     private final RepresentativeImageRepository representativeImageRepository;
     private final S3Service s3Service;
 
+    @Transactional
     public RepresentativeImageResponse createRepresentativeImage(RepresentativeImageRequest representativeImageRequest, User user) throws IOException {
         String representativeImageUrl = this.s3Service.upload(representativeImageRequest.getImage(), user.getNickname());
         RepresentativeImage newRepresentativeImage = RepresentativeImage.builder().region(representativeImageRequest.getRegion())
@@ -34,5 +38,12 @@ public class RepresentativeImageService {
                 .stream()
                 .map(RepresentativeImageResponse::new)
                 .toList();
+    }
+
+    @Transactional
+    public void updateRepresentativeImage(Long id, MultipartFile image, User user) throws IOException {
+        String newImageUrl = this.s3Service.upload(image, user.getNickname());
+        RepresentativeImage representativeImage = this.representativeImageRepository.findById(id).orElseThrow();
+        representativeImage.updateUrl(newImageUrl);
     }
 }
