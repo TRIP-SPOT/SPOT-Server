c69c6a7627a81443c6fbbb78b2baab6da7608117
yang <hc1489@naver.com>
2024-09-16 23:41:09 +0900

    feat: 퀴즈 조회 기능 url 매핑

--- src/main/java/com/spot/spotserver/api/quiz/controller/QuizController.java
+++ src/main/java/com/spot/spotserver/api/quiz/controller/QuizController.java
@@ -1,7 +1,21 @@
 package com.spot.spotserver.api.quiz.controller;
 
+import com.spot.spotserver.api.quiz.dto.QuizResponse;
+import com.spot.spotserver.api.quiz.service.QuizService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/quiz")
 public class QuizController {
+
+    private final QuizService quizService;
+
+    @GetMapping()
+    public QuizResponse getQuiz(Long id) {
+        return this.quizService.getQuiz(id);
+    }
 }
