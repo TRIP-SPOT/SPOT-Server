c6e39b51f2204cb1db40700643f8b5a48148c2ff
yang <hc1489@naver.com>
2024-09-17 21:19:24 +0900

    feat: BadgeRepository findAllByUserAndRegion 인터페이스 작성

--- src/main/java/com/spot/spotserver/api/quiz/repository/BadgeRepository.java
+++ src/main/java/com/spot/spotserver/api/quiz/repository/BadgeRepository.java
@@ -1,7 +1,12 @@
 package com.spot.spotserver.api.quiz.repository;
 
 import com.spot.spotserver.api.quiz.domain.Badge;
+import com.spot.spotserver.api.record.domain.Region;
+import com.spot.spotserver.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BadgeRepository extends JpaRepository<Badge, Long> {
+    Optional<Badge> findAllByUserAndRegion(User user, Region region);
 }
