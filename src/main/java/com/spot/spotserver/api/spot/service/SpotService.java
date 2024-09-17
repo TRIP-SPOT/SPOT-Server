0054524061501880c9a6d2aa59a8f86363da7583
Jeongmin39 <wjdals20309@naver.com>
2024-09-17 20:09:23 +0900

    feat: SpotService 상세정보 조회 메소드 작성

--- src/main/java/com/spot/spotserver/api/spot/service/SpotService.java
+++ src/main/java/com/spot/spotserver/api/spot/service/SpotService.java
@@ -1,7 +1,35 @@
 package com.spot.spotserver.api.spot.service;
 
+import com.spot.spotserver.api.spot.client.CommonInfoClient;
+import com.spot.spotserver.api.spot.dto.response.CommonInfoResponse;
+import com.spot.spotserver.api.spot.dto.response.SpotDetailsResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SpotService {
+
+    private final CommonInfoClient commonInfoClient;
+
+    @Value("${tourApi.serviceKey}")
+    private String serviceKey;
+    private static final String mobileOS = "AND";
+    private static final String mobileApp = "SPOT";
+    private static final String _type = "json";
+
+    public SpotDetailsResponse getSpotDetails(Integer contentId) {
+
+        CommonInfoResponse commonInfo = commonInfoClient.getCommonInfo(
+                mobileOS, mobileApp, _type, contentId.toString(), "Y", "Y", "Y", "Y", "Y", serviceKey
+        );
+
+        if (commonInfo == null || commonInfo.response().body().items().item().isEmpty()) {
+            throw new IllegalArgumentException("데이터가 없습니다.");
+        }
+
+        SpotDetailsResponse spotDetailsResponse = SpotDetailsResponse.fromCommonInfo(commonInfo.response().body().items().item().get(0));
+        return spotDetailsResponse;
+    }
 }
