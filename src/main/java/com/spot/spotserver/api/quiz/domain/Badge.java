f4f3463d800d6cfd07cfa700096c43c2a31de978
yang <hc1489@naver.com>
2024-09-17 21:18:13 +0900

    feat: Badge Entity 뱃지 추가 로직 구현

--- src/main/java/com/spot/spotserver/api/quiz/domain/Badge.java
+++ src/main/java/com/spot/spotserver/api/quiz/domain/Badge.java
@@ -1,21 +1,39 @@
 package com.spot.spotserver.api.quiz.domain;
 
+import com.spot.spotserver.api.record.domain.Region;
 import com.spot.spotserver.api.user.domain.User;
 import com.spot.spotserver.common.domain.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "badge")
+@Getter
+@NoArgsConstructor
 public class Badge extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private Region region;
 
-    private String imageUrl;
+    private Integer count = 0;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Builder
+    public Badge(Long id, Region region, User user) {
+        this.id = id;
+        this.region = region;
+        this.user = user;
+    }
+
+    public void addBadge() {
+        this.count++;
+    }
 }
