197f3ad5dc37d2d951cd2f0143414e0efc059aeb
Jeongmin39 <wjdals20309@naver.com>
2024-09-17 20:08:55 +0900

    feat: 스팟 상세정보 조회 관련 DTO 생성

---
+++ src/main/java/com/spot/spotserver/api/spot/dto/response/SpotDetailsResponse.java
@@ -0,0 +1,33 @@
+package com.spot.spotserver.api.spot.dto.response;
+
+public record SpotDetailsResponse(
+        String contentId,
+        String contentTypeId,
+        String title,
+        String image,
+        String homepage,
+        String tel,
+        String addr1,       // 주소
+        String addr2,       // 상세주소
+        String zipcode,     // 우편번호
+        String longtitude,
+        String latitude,
+        String overview
+) {
+    public static SpotDetailsResponse fromCommonInfo(CommonInfoResponse.Item item) {
+        return new SpotDetailsResponse(
+                item.contentid(),
+                item.contenttypeid(),
+                item.title(),
+                item.firstimage() != null ? item.firstimage() : item.firstimage2(), // 첫 번째 이미지가 없으면 대체 이미지 사용
+                item.homepage(),
+                item.tel(),
+                item.addr1(),
+                item.addr2(),
+                item.zipcode(),
+                item.mapx(),
+                item.mapy(),
+                item.overview()
+        );
+    }
+}
