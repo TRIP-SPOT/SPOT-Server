4c8cf5ed354a055d345d527e513cc9f43f00b54b
yang <hc1489@naver.com>
2024-09-19 00:49:23 +0900

    feat: 여행 기록 삭제 기능 url 매핑

--- src/main/java/com/spot/spotserver/api/schedule/controller/ScheduleController.java
+++ src/main/java/com/spot/spotserver/api/schedule/controller/ScheduleController.java
@@ -8,10 +8,7 @@ import com.spot.spotserver.common.annotation.CurrentUser;
 import com.spot.spotserver.common.payload.ApiResponse;
 import com.spot.spotserver.common.payload.SuccessCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -29,5 +26,11 @@ public class ScheduleController {
                                                         @CurrentUser User user) throws IOException {
         ScheduleResponse scheduleResponse = this.scheduleService.createSchedule(scheduleRequest, image, user);
         return ApiResponse.success(SuccessCode.CREATE_SCHEDULE_SUCCESS, scheduleResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public <T> ApiResponse<T> deleteSchedule(@PathVariable Long id) {
+        this.scheduleService.deleteSchedule(id);
+        return ApiResponse.success(SuccessCode.DELETE_SCHEDULE_SUCCESS);
     }
 }
