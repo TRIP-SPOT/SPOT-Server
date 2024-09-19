137f5cd20b1e3ae98f231404d1c79649efd3c20f
Jeongmin39 <wjdals20309@naver.com>
2024-09-19 22:16:31 +0900

    setting: ErrorCode 추가

--- src/main/java/com/spot/spotserver/common/payload/ErrorCode.java
+++ src/main/java/com/spot/spotserver/common/payload/ErrorCode.java
@@ -5,8 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -14,12 +13,21 @@ public enum ErrorCode {
 
     // 400 BAD REQUEST
     REQUEST_VALIDATION_EXCEPTION(BAD_REQUEST, "잘못된 요청입니다."),
+    INVALID_AUTH_CODE(BAD_REQUEST, "인가 코드가 유효하지 않습니다."),
+    TOKEN_REQUEST_FAILED(BAD_REQUEST, "액세스 토큰 요청에 실패했습니다."),
+    USER_INFO_REQUEST_FAILED(BAD_REQUEST, "사용자 정보 요청에 실패했습니다."),
+    USER_CREATION_FAILED(BAD_REQUEST, "사용자 생성에 실패했습니다."),
 
     // 401 UNAUTHORIZED
+    EMPTY_JWT(UNAUTHORIZED, "JWT 토큰이 비어 있습니다."),
+    INVALID_JWT_TOKEN(UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다."),
+    EXPIRED_JWT_TOKEN(UNAUTHORIZED, "JWT 토큰이 만료되었습니다."),
+    UNSUPPORTED_JWT_TOKEN(UNAUTHORIZED, "지원하지 않는 JWT 토큰 형식입니다."),
 
     // 403 FORBIDDEN
 
     // 404 NOT FOUND
+    USER_NOT_FOUND(NOT_FOUND, "해당하는 사용자가 존재하지 않습니다."),
     RECORD_NOT_FOUND(NOT_FOUND, "해당하는 기록이 존재하지 않습니다."),
     RECORD_IMAGE_NOT_FOUND(NOT_FOUND, "해당하는 기록 이미지가 존재하지 않습니다."),
     REPRESENTATIVE_IMAGE_NOT_FOUND(NOT_FOUND, "해당하는 대표 이미지가 존재하지 않습니다."),
