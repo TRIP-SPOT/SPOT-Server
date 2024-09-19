0458df0923b59c8471a0ad616aa70fa0ac227257
yang <hc1489@naver.com>
2024-09-19 22:58:24 +0900

    feat: 여행 일정 기간 수정 기능 url 매핑

--- src/main/java/com/spot/spotserver/api/schedule/controller/ScheduleController.java
+++ src/main/java/com/spot/spotserver/api/schedule/controller/ScheduleController.java
@@ -1,5 +1,7 @@
 package com.spot.spotserver.api.schedule.controller;
 
+import com.spot.spotserver.api.schedule.dto.ScheduleDurationUpdateRequest;
+import com.spot.spotserver.api.schedule.dto.ScheduleDurationUpdateResponse;
 import com.spot.spotserver.api.schedule.dto.ScheduleRequest;
 import com.spot.spotserver.api.schedule.dto.ScheduleResponse;
 import com.spot.spotserver.api.schedule.service.ScheduleService;
@@ -7,6 +9,7 @@ import com.spot.spotserver.api.user.domain.User;
 import com.spot.spotserver.common.annotation.CurrentUser;
 import com.spot.spotserver.common.payload.ApiResponse;
 import com.spot.spotserver.common.payload.SuccessCode;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,5 +35,12 @@ public class ScheduleController {
     public <T> ApiResponse<T> deleteSchedule(@PathVariable Long id) {
         this.scheduleService.deleteSchedule(id);
         return ApiResponse.success(SuccessCode.DELETE_SCHEDULE_SUCCESS);
+    }
+
+    @PatchMapping("/{id}")
+    public ApiResponse<ScheduleDurationUpdateResponse> updateDuration(@PathVariable Long id,
+                                                                      @RequestBody @Valid ScheduleDurationUpdateRequest scheduleDurationUpdateRequest) {
+        ScheduleDurationUpdateResponse scheduleDurationUpdateResponse = this.scheduleService.updateDuration(id, scheduleDurationUpdateRequest);
+        return ApiResponse.success(SuccessCode.UPDATE_SCHEDULE_DURATION_SUCCESS, scheduleDurationUpdateResponse);
     }
 }
