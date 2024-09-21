787ea5e802530633afad33db34e94cb9ec3d9903
yang <hc1489@naver.com>
2024-09-21 14:56:56 +0900

    feat: LocationRepository day, seq 계산을 위한 메서드 추가

--- src/main/java/com/spot/spotserver/api/schedule/repository/LocationRepository.java
+++ src/main/java/com/spot/spotserver/api/schedule/repository/LocationRepository.java
@@ -1,7 +1,10 @@
 package com.spot.spotserver.api.schedule.repository;
 
 import com.spot.spotserver.api.schedule.domain.Location;
+import com.spot.spotserver.api.schedule.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    Integer countBySchedule(Schedule schedule);
+    Integer countByScheduleAndDay(Schedule schedule, Integer day);
 }
