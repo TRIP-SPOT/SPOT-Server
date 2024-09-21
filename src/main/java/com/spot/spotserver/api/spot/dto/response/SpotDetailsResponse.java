fe0f7b33f6d6c05276c08f6094e622e1d62a4c2a
Jeongmin39 <wjdals20309@naver.com>
2024-09-22 04:18:34 +0900

    feat: SpotDetailsResponse에 region, city 필드 추가

--- src/main/java/com/spot/spotserver/api/spot/dto/response/SpotDetailsResponse.java
+++ src/main/java/com/spot/spotserver/api/spot/dto/response/SpotDetailsResponse.java
@@ -1,5 +1,13 @@
 package com.spot.spotserver.api.spot.dto.response;
 
+
+import com.spot.spotserver.api.spot.domain.Spot;
+import com.spot.spotserver.api.spot.exception.SpotNotFoundException;
+import com.spot.spotserver.api.spot.repository.SpotRepository;
+import com.spot.spotserver.common.payload.ErrorCode;
+
+import java.util.Optional;
+
 public record SpotDetailsResponse(
         String contentId,
         String contentTypeId,
@@ -10,11 +18,17 @@ public record SpotDetailsResponse(
         String addr1,       // 주소
         String addr2,       // 상세주소
         String zipcode,     // 우편번호
+        Integer region,
+        Integer city,
         String longitude,
         String latitude,
         String overview
 ) {
-    public static SpotDetailsResponse fromCommonInfo(CommonInfoResponse.Item item) {
+    public static SpotDetailsResponse fromCommonInfo(CommonInfoResponse.Item item, SpotRepository spotRepository) {
+
+        Optional<Spot> spot = Optional.ofNullable(spotRepository.findByContentId(Integer.parseInt(item.contentid()))
+                .orElseThrow(() -> new SpotNotFoundException(ErrorCode.SPOT_NOT_FOUND)));
+
         return new SpotDetailsResponse(
                 item.contentid(),
                 item.contenttypeid(),
@@ -25,6 +39,8 @@ public record SpotDetailsResponse(
                 item.addr1(),
                 item.addr2(),
                 item.zipcode(),
+                spot.get().getRegion() != null ? spot.get().getRegion().ordinal() : null,
+                spot.get().getCity() != null ? spot.get().getCity().ordinal() : null,
                 item.mapx(),
                 item.mapy(),
                 item.overview()
