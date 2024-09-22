90ef50fd3961c3f4c7429d214da2265c3e8e48a4
yang <hc1489@naver.com>
2024-09-22 21:54:38 +0900

    feat: 일정의 담은 Spot DTO 작성

---
+++ src/main/java/com/spot/spotserver/api/schedule/dto/response/SelectedSpotResponse.java
@@ -0,0 +1,31 @@
+package com.spot.spotserver.api.schedule.dto.response;
+
+import com.spot.spotserver.api.schedule.domain.SelectedSpot;
+import lombok.Data;
+
+@Data
+public class SelectedSpotResponse {
+    private String title;
+
+    private String addr1;
+
+    private String addr2;
+
+    private String contentId;
+
+    private String contentTypeId;
+
+    private String dist;
+
+    private String image;
+
+    public SelectedSpotResponse(SelectedSpot spot) {
+        this.title = spot.getTitle();
+        this.addr1 = spot.getAddr1();
+        this.addr2 = spot.getAddr2();
+        this.contentId = spot.getContentId();
+        this.contentTypeId = spot.getContentTypeId();
+        this.dist = spot.getDist();
+        this.image = spot.getImage();
+    }
+}
