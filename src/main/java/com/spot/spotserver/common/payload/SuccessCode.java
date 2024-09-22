edca95951e4ca8e0d0f5433287a837309c46c46a
Jeongmin39 <wjdals20309@naver.com>
2024-09-23 03:49:03 +0900

    setting: SuccessCode 추가

--- src/main/java/com/spot/spotserver/common/payload/SuccessCode.java
+++ src/main/java/com/spot/spotserver/common/payload/SuccessCode.java
@@ -33,6 +33,7 @@ public enum SuccessCode {
     GET_WITHIN_RADIUS_SPOT_LIST_SUCCESS(OK, "반경 20km 이내의 스팟 목록이 정상적으로 조회되었습니다."),
     LIKE_SPOT_SUCCESS(OK, "스팟에 대한 좋아요가 정상적으로 등록되었습니다."),
     UNLIKE_SPOT_SUCCESS(OK, "스팟에 대한 좋아요가 정상적으로 삭제되었습니다."),
+    GET_TOP_LIKE_SPOT_SUCCESS(OK, "좋아요 상위 5개 촬영지가 정상적으로 조회되었습니다."),
 
     // RECORD
     CREATE_RECORD_SUCCESS(OK, "여행 기록이 정상적으로 생성되었습니다."),
