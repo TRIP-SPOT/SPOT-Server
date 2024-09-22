a7593cf5168134a169c574bd6584f05a188ce91b
Jeongmin39 <wjdals20309@naver.com>
2024-09-22 18:33:03 +0900

    feat: LikesRepository findByUserAndSpot 추가

--- src/main/java/com/spot/spotserver/api/spot/repository/LikesRepository.java
+++ src/main/java/com/spot/spotserver/api/spot/repository/LikesRepository.java
@@ -1,7 +1,10 @@
 package com.spot.spotserver.api.spot.repository;
 
 import com.spot.spotserver.api.spot.domain.Likes;
+import com.spot.spotserver.api.spot.domain.Spot;
+import com.spot.spotserver.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
+    Likes findByUserAndSpot(User user, Spot spot);
 }
