ddb7c4fdbeb764c116460578b2980f10f83f25f9
yang <hc1489@naver.com>
2024-09-23 16:41:33 +0900

    refac: SpotType Enum 작성

---
+++ src/main/java/com/spot/spotserver/common/domain/SpotType.java
@@ -0,0 +1,14 @@
+package com.spot.spotserver.common.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum SpotType {
+    ATTRACTION("12"),
+    RESTAURANT("39"),
+    ACCOMMODATION("32");
+
+    private final String type;
+
+    private String type() {return type;}
+}
