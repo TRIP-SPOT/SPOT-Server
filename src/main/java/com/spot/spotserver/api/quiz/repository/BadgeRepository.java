3b1fae62e2b0cc3c71bc45c0894bd1a853cb55f4
Jeongmin39 <wjdals20309@naver.com>
2024-09-25 22:52:57 +0900

    feat: BadgeRepository findByUser 추가

--- src/main/java/com/spot/spotserver/api/quiz/repository/BadgeRepository.java
+++ src/main/java/com/spot/spotserver/api/quiz/repository/BadgeRepository.java
@@ -5,8 +5,10 @@ import com.spot.spotserver.common.domain.Region;
 import com.spot.spotserver.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BadgeRepository extends JpaRepository<Badge, Long> {
     Optional<Badge> findAllByUserAndRegion(User user, Region region);
+    List<Badge> findByUser(User user);
 }
