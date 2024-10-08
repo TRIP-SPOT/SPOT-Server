cf877959e01aec52680c273e7f1f3e8d174faa2f
Jeongmin39 <wjdals20309@naver.com>
2024-10-08 19:03:12 +0900

    feat: 작품명 검색 쿼리에서 공백 및 특수문자 처리 기능 추가

--- src/main/java/com/spot/spotserver/api/spot/repository/WorkRepository.java
+++ src/main/java/com/spot/spotserver/api/spot/repository/WorkRepository.java
@@ -2,9 +2,12 @@ package com.spot.spotserver.api.spot.repository;
 
 import com.spot.spotserver.api.spot.domain.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface WorkRepository extends JpaRepository<Work, Long> {
-    List<Work> findByNameContainingIgnoreCase(String workName);
+    @Query("SELECT w FROM Work w WHERE REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(w.name, ' ', ''), '!', ''), '?', ''), ',', ''), '-', ''), ':', '') " +
+            "LIKE CONCAT('%', REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(:workName, ' ', ''), '!', ''), '?', ''), ',', ''), '-', ''), ':', ''), '%')")
+    List<Work> searchWorksByName(String workName);
 }
