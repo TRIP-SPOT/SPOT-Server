16f192cac2bb216a97c5f05bcf67612c3058ed58
Jeongmin39 <wjdals20309@naver.com>
2025-01-03 17:21:05 +0900

    chore: 불필요한 import문 삭제

--- src/main/java/com/spot/spotserver/api/auth/jwt/redis/TokenRepository.java
+++ src/main/java/com/spot/spotserver/api/auth/jwt/redis/TokenRepository.java
@@ -2,7 +2,5 @@ package com.spot.spotserver.api.auth.jwt.redis;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface TokenRepository extends CrudRepository<Token, String> {
 }
